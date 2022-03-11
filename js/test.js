c5e3999efd814e317bf58f2441de6b892bf10269
MohamedMamdouh-98 <miido96@gmail.com>
2022-03-11 16:27:41 +0200

    test

---
+++ js/test.js
@@ -0,0 +1,9 @@
+let images = document.querySelectorAll('img'),
+buttons = document.querySelectorAll('button'),
+productNumber = document.getElementById('productNum'),
+price = document.getElementById('price');
+
+let newData = [{title: home1, price: 10} , {title: home2, price:20} , {title: home3, price:30}]
+
+for(let i = 0; i < newData.length; i++){
+}
